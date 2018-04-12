75527aa41aa288230321a0888d299926c34dbf95
Theron Halsey <theron.halsey@gmail.com>
2018-04-12 19:25:31 -0400

    weather user

---
+++ weatherAdmin/newUser.js
@@ -0,0 +1,14 @@
+var weather = require('weather-js');
+
+var User = function (name, location) {
+  this.name = name;
+  this.location = location;
+  this.date = Date.now();
+  this.getWeather = function(){
+    weather.find({search: this.location, degreeType: 'F'}, function(err, result) {
+  if(err) console.log(err);
+ 
+  console.log(JSON.stringify(result, null, 2));
+});
+  }
+}
