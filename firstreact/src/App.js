e8ec6dea5e3a1fb941c47ca4e90f15d4a68693d3
Theron Halsey <theron.halsey@gmail.com>
2018-06-12 20:36:25 -0400

    more react

--- firstreact/src/App.js
+++ firstreact/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
+import HelloBootstrap from "./components/HelloBootstrap";
 
-const App = () => <p>Render Bootstrap components here</p>;
+const App = () => <HelloBootstrap />;
+
 
 export default App;
--- firstreact/src/components/HelloBootstrap.js
+++ firstreact/src/components/HelloBootstrap.js
@@ -3,28 +3,30 @@ import React from "react";
 const HelloBootstrap = () => (
   <div className="container">
     <div className="jumbotron">
-      <h1>Hello, world!</h1>
-      <p>I'm a jumbotron!</p>
+      <h1>PARTY WORLD CHAMPIONSHIP!</h1>
+      <p>DO YOU HAVE WHAT IT TAKES TO ENTER THE PARTY BATTLE!?!</p>
       <p>
         <button className="btn btn-primary btn-lg">
-          Learn more
+          PARTY HARD
         </button>
       </p>
     </div>
     <div className="card mb-4">
       <div className="card-header">
-        <h3>Card title</h3>
+        <h3>ANDREW W. K.</h3>
       </div>
       <div className="card-body">
-        <p className="card-text">Card content</p>
+        <p className="card-text">CURRENT PARTYING WORLD CHAMPION</p>
+        <img src="http://andrewwk.com/cms/andrewwk/news/5bdec47d8b5d5d39b901b2bb15d2782b.jpg"/>
       </div>
     </div>
     <div className="card mb-4">
       <div className="card-header">
-        <h3>Card title</h3>
+        <h3>SPUDS MCKENZIE</h3>
       </div>
       <div className="card-body">
-        <p className="card-text">Card content</p>
+        <p className="card-text">PARTIEST DOG ON EARTH</p>
+        <img src="http://static3.businessinsider.com/image/58935b5b6e09a826008b49a3-812/screen%20shot%202017-02-02%20at%20111559%20am.png"/>
       </div>
     </div>
   </div>
