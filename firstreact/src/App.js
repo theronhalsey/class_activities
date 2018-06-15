b0985055856ab67b2b94ec688d11705e3a4f2c09
Theron Halsey <theron.halsey@gmail.com>
2018-06-14 21:25:24 -0400

    react state activity 2

--- firstreact/src/App.js
+++ firstreact/src/App.js
@@ -1,6 +1,23 @@
 import React from "react";
-import Counter from "./components/Counter";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import friends from "./friends.json";
+import "./App.css";
 
-const App = () => <Counter />;
+const App = () => (
+  <Wrapper>
+    <h1 className="title">Friends List</h1>
+    {this.state.friends.map(item => (
+    <FriendCard
+        key={item.id}
+        name={item.name}
+        image={item.image}
+        occupation={item.occupation}
+        location={item.location}
+        />
+      ))}
+      
+    </Wrapper>
+);
 
 export default App;
