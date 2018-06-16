9225c5063fe5fd2dcecda073f373f1d24117b6bb
Theron Halsey <theron.halsey@gmail.com>
2018-06-16 11:19:27 -0400

    react ajax

--- firstreact/src/App.js
+++ firstreact/src/App.js
@@ -1,23 +1,6 @@
 import React from "react";
-import FriendCard from "./components/FriendCard";
-import Wrapper from "./components/Wrapper";
-import friends from "./friends.json";
-import "./App.css";
+import OmdbContainer from "./components/OmdbContainer";
 
-const App = () => (
-  <Wrapper>
-    <h1 className="title">Friends List</h1>
-    {this.state.friends.map(item => (
-    <FriendCard
-        key={item.id}
-        name={item.name}
-        image={item.image}
-        occupation={item.occupation}
-        location={item.location}
-        />
-      ))}
-      
-    </Wrapper>
-);
+const App = () => <OmdbContainer />;
 
 export default App;
